d85db423891692ebf58b9cd5442b03f76d23205f
qjz07254 <qjz07254@LY.com>
2014-11-18 17:10:32 +0800

    广告

---
+++ 30Dal/Hot371.Dal/Sys/SysAdvertisingRespository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hot371.Model;
+using TCBase.Saker.Core.Persistence;
+
+namespace Hot371.Dal.Sys
+{
+    public class SysAdvertisingRespository : Respository<SysAdvertising>
+    {
+        public SysAdvertisingRespository()
+            :base(DBGlobal.Hot371.ToString())
+        { }
+    }
+}
---
+++ 40Bll/Hot371.Bll/Sys/SysAdvertisingBiz.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hot371.Dal.Sys;
+using Hot371.Model;
+using QJZ.Framework.Utility;
+
+namespace Hot371.Bll.Sys
+{
+    public class SysAdvertisingBiz
+    {
+        /// <summary>
+        /// 缓存key
+        /// </summary>
+        private const string CacheKey = "Hot371.Advertising";
+
+        private readonly SysAdvertisingRespository _respository;
+
+        public SysAdvertisingBiz()
+        {
+            this._respository = new SysAdvertisingRespository();
+        }
+
+        /// <summary>
+        /// 获取全部放缓存 1小时
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<SysAdvertising> GetAll()
+        {
+            var cache = Caching.Get(CacheKey) as IEnumerable<SysAdvertising>;
+            if (cache != null)
+            {
+                return cache;
+            }
+
+            var all = _respository.GetAll();
+            try
+            {
+                Caching.Set(CacheKey, all, 60);
+            }
+            catch (Exception)
+            {
+            }
+            return all;
+        }
+
+        public List<SysAdvertising> GetCityAdvertising(int cityId)
+        {
+            var cities = new List<int?> {0, cityId};
+            var all = GetAll();
+            var advs = all.Where(a => cities.Contains(a.CityId)).ToList();
+            return advs;
+        }
+    }
+}
