b3e27bd5c8b1a20c97c5683d3b617f07455b0e1f
qjz07254 <qjz07254@LY.com>
2014-11-20 17:46:03 +0800

    文件上传

--- 50App/Hot371.Site.Mobile/Controllers/HomeController.cs
+++ 50App/Hot371.Site.Mobile/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,9 +15,37 @@ namespace Hot371.Site.Mobile.Controllers
 
         public ActionResult Index()
         {
-            var test = new SysAreaBiz().GetAreaByLevel(1);
-            var test2 = new SysAreaBiz().GetAreaByParent(320500);
+            //var test = new SysAreaBiz().GetAreaByLevel(1);
+            //var test2 = new SysAreaBiz().GetAreaByParent(320500);
             return View();
+        }
+
+        public ActionResult ImageUpload(string fileStream)
+        {
+            var base64 = fileStream.Split(',')[1];
+            byte[] file = Convert.FromBase64String(base64);
+            var uploadPath = HttpContext.Server.MapPath("~/" + "Upload");
+            uploadPath = Path.Combine(uploadPath, "Ent");
+            var filePath = Path.Combine(uploadPath,
+                    string.Format("{0}{1}.{2}", DateTime.Now.ToString("yyyyMMddhhmmss"), new Random(DateTime.Now.Millisecond).Next(10000), "jpg")
+                    );
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+            using (var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                fs.Write(file, 0, file.Length);
+                fs.Flush();
+                fs.Close();
+            }
+
+            var result = new
+            {
+                State = 200,
+                ImgUrl = filePath
+            };
+            return Json(result);
         }
 
     }
